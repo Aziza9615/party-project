f593e57eec6b105132182bdcb5fe39bb2517640f
Aziza9615 <azizisadykova@gmail.com>
2021-09-20 16:45:21 +0600

    I deleted the activation of languages , made a choice of languages

--- app/src/main/java/com/example/authactivity/ui/emblem/EmblemActivity.kt
+++ app/src/main/java/com/example/authactivity/ui/emblem/EmblemActivity.kt
@@ -1,21 +1,34 @@
 package com.example.authactivity.ui.emblem
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
-import androidx.lifecycle.Observer
+import android.content.res.Configuration
+import android.widget.Button
+import com.example.authactivity.R
 import com.example.authactivity.base.BaseActivity
 import com.example.authactivity.databinding.ActivityEmblemBinding
-import com.example.authactivity.ui.language.LanguageActivity
 import com.example.authactivity.ui.main.MainActivity
 import com.example.authactivity.ui.main.MainViewModel
 import org.koin.androidx.viewmodel.ext.android.getViewModel
+import java.util.*
 
 class EmblemActivity : BaseActivity<MainViewModel, ActivityEmblemBinding>(MainViewModel::class) {
+
+    lateinit var mBtn : Button
 
     override fun getViewBinding() = ActivityEmblemBinding.inflate(layoutInflater)
 
     override fun setupViews() {
         viewModel = getViewModel(clazz = MainViewModel::class)
         setupListener()
+        loadLocate()
+
+        mBtn = findViewById(R.id.language)
+        mBtn.setOnClickListener {
+            showChangeLang()
+        }
     }
 
     private fun setupListener() {
@@ -23,10 +36,50 @@ class EmblemActivity : BaseActivity<MainViewModel, ActivityEmblemBinding>(MainVi
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        binding.language.setOnClickListener {
-            val intent = Intent(this, LanguageActivity::class.java)
-            startActivity(intent)
+    }
+
+    private fun showChangeLang() {
+        val listItems = arrayOf("English", "Кыргызский", "Русский")
+
+        val mBuilder = AlertDialog.Builder(this@EmblemActivity)
+        mBuilder.setTitle("Choose Language")
+        mBuilder.setSingleChoiceItems(listItems, -1) { dialog, which ->
+            if (which == 0) {
+                setLocate("en")
+                recreate()
+
+            } else if (which == 1) {
+                setLocate("ky")
+                recreate()
+
+            } else if (which == 2) {
+                setLocate("ru")
+                recreate()
+            }
+
+            dialog.dismiss()
         }
+
+        val mDialog = mBuilder.create()
+        mDialog.show()
+    }
+
+    private fun setLocate(Lang: String?) {
+        val locale = Locale(Lang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", Lang)
+        editor.apply()
+    }
+
+    private fun loadLocate() {
+        val SharedPreferences =  getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        val language = SharedPreferences.getString("My_Lang", "")
+        setLocate(language)
     }
 
     override fun subscribeToLiveData() {
