0cbc5053425113a79b84dbc7005abc8b5ef430fa
Aziza961531 <73041910+Aziza9615@users.noreply.github.com>
2021-11-24 03:00:49 +0600

    the correct code with the addition of OnBoarding and Dot Indicator

--- app/src/main/java/com/example/authactivity/di/Koin.kt
+++ app/src/main/java/com/example/authactivity/di/Koin.kt
@@ -3,8 +3,6 @@ package com.example.authactivity.di
 import com.example.authactivity.local.PrefsHelper
 import com.example.authactivity.ui.lang.LangViewModel
 import com.example.authactivity.ui.main.MainViewModel
-import com.example.authactivity.ui.onBoard.OnBoard2ViewModel
-import com.example.authactivity.ui.onBoard.OnBoard3ViewModel
 import com.example.authactivity.ui.onBoard.OnBoardViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -17,8 +15,8 @@ val viewModelModule = module {
     viewModel { MainViewModel() }
     viewModel { LangViewModel() }
     viewModel { OnBoardViewModel() }
-    viewModel { OnBoard2ViewModel() }
-    viewModel { OnBoard3ViewModel() }
+//    viewModel { OnBoard2ViewModel() }
+//    viewModel { OnBoard3ViewModel() }
 }
 
 val repositoryModule = module {
--- app/src/main/java/com/example/authactivity/ui/onBoard/OnBoardActivity.kt
+++ app/src/main/java/com/example/authactivity/ui/onBoard/OnBoardActivity.kt
@@ -1,39 +1,107 @@
 package com.example.authactivity.ui.onBoard
 
-import android.content.ClipDescription
 import android.content.Intent
-import android.icu.text.CaseMap
-import android.widget.Button
+import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.example.authactivity.R
-import com.example.authactivity.base.BaseActivity
 import com.example.authactivity.base.BaseEvent
 import com.example.authactivity.base.BaseViewModel
-import com.example.authactivity.databinding.ActivityOnboardBinding
 import com.example.authactivity.ui.lang.LangActivity
-import com.example.authactivity.ui.lang.LangViewModel
+import com.example.authactivity.ui.main.MainActivity
 import kotlinx.android.synthetic.main.activity_onboard.*
-import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class OnBoardViewModel : BaseViewModel<BaseEvent>()
-class OnBoardActivity : BaseActivity<OnBoardViewModel, ActivityOnboardBinding>(OnBoardViewModel::class) {
+class OnBoardActivity : AppCompatActivity() {
 
-    override fun getViewBinding() = ActivityOnboardBinding.inflate(layoutInflater)
+    private lateinit var sliderAdapter: SliderAdapter
+    private var dots: Array<TextView?>? = null
+    private lateinit var layouts: Array<Int>
+    private val sliderChangeListener = object : ViewPager.OnPageChangeListener {
 
-    override fun setupViews() {
-        viewModel = getViewModel(clazz = OnBoardViewModel::class)
+        override fun onPageSelected(position: Int) {
+            addBottomDots(position)
+            if (position == layouts.size.minus(1)) {
+                nextBtn.show()
+            } else {
+                nextBtn.show()
+            }
+        }
+
+        override fun onPageScrollStateChanged(state: Int) {
+        }
+
+        override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboard)
+        init()
+        dataSet()
+        interactions()
         setupListener()
     }
 
     private fun setupListener() {
-        binding.arrow.setOnClickListener {
-            startActivity(Intent(this@OnBoardActivity, LangActivity::class.java))
-        }
-            binding.goBtn.setOnClickListener {
-                startActivity(Intent(this@OnBoardActivity,OnBoard2Activity::class.java))
+        arrow_btn.setOnClickListener {
+            val intent = Intent(this, LangActivity::class.java)
+            startActivity(intent)
         }
     }
 
-    override fun subscribeToLiveData() {
+    private fun init() {
+        layouts = arrayOf(
+                R.layout.onboarding_slide1,
+                R.layout.onboarding_slide2,
+                R.layout.onboarding_slide3
+        )
+        sliderAdapter = SliderAdapter(this, layouts)
     }
 
+    private fun dataSet() {
+        addBottomDots(0)
+        slider.apply {
+            adapter = sliderAdapter
+            addOnPageChangeListener(sliderChangeListener)
+        }
+    }
+
+    private fun interactions() {
+        nextBtn.setOnClickListener {
+            val current = getCurrentScreen(+1)
+            if (current < layouts.size) {
+                slider.currentItem = current
+            } else {
+                onClick()
+            }
+        }
+    }
+
+    private fun onClick() {
+        AppPrefs(this).setFirstTimeLaunch(false)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun addBottomDots(currentPage: Int) {
+        dots = arrayOfNulls(layouts.size)
+        dotsLayout.removeAllViews()
+        for (i in 0 until dots!!.size) {
+            dots!![i] = TextView(this)
+            dots!![i]?.text = Html.fromHtml("&#8226;")
+            dots!![i]?.textSize = 35f
+            dots!![i]?.setTextColor(resources.getColor(R.color.brand_back))
+            dotsLayout.addView(dots!![i])
+        }
+
+        if (dots!!.isNotEmpty()) {
+            dots!![currentPage]?.setTextColor(resources.getColor(R.color.brand))
+        }
+    }
+
+    private fun getCurrentScreen(i: Int): Int = slider.currentItem.plus(i)
 }
